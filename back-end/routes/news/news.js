2370e752b2d3c65dd1260d68339f28c1f85f4980
chino4242 <ryanj.contino@gmail.com>
2025-07-22 15:14:19 -0400

    news GET endpoint is working

--- back-end/routes/news/news.js
+++ back-end/routes/news/news.js
@@ -1,8 +1,51 @@
 import express from "express";
 const router = express.Router();
+const baseUrl = "https://newsapi.org/v2/top-headlines";
+
+const apiKey = process.env.API_KEY;
+if (!apiKey) {
+  console.log(
+    "Please set the API_KEY environment variable with a valid newsapi.org apiKey and restart the server!"
+  );
+  process.exit(0);
+}
+
+function addApiKey(queryObject) {
+  return { ...queryObject, apiKey: apiKey };
+}
+
+export function createUrlFromQueryObject(queryObjectWithApiKey) {
+  const queryString = new URLSearchParams(queryObjectWithApiKey).toString();
+  const url = baseUrl + "?" + queryString;
+  return url;
+}
+
+export async function fetchData(url) {
+  let data = null;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return null;
+  }
+}
 
 router.get("/", async (req, res) => {
-  res.send("Get called");
+  let fixedQueryObject = {
+    country: "us",
+    q: "news",
+  };
+  let queryObject = addApiKey(fixedQueryObject);
+
+  const url = createUrlFromQueryObject(queryObject);
+  const newsArticles = await fetchData(url);
+
+  res.send(newsArticles);
 });
 
 router.post("/", async (req, res) => {
